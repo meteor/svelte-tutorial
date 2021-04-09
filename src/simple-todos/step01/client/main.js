144a60d5ef3e9d0e737597205b0d8a5a127a0807
denyhs <denilsonh2014@gmail.com>
2021-04-09 11:44:35 -0400

    #606 - Svelte Tutorial
    - step 01 init

--- src/simple-todos/step01/client/main.js
+++ src/simple-todos/step01/client/main.js
@@ -1,1 +1,9 @@
-import '../imports/ui/App.js';
+import { Meteor } from 'meteor/meteor';
+import App from '../imports/ui/App.svelte';
+
+Meteor.startup(() => {
+  // eslint-disable-next-line no-new
+  new App({
+    target: document.getElementById('app'),
+  });
+});
--- src/simple-todos/step02/imports/ui/App.js
+++
@@ -1,9 +0,0 @@
-import { Template } from 'meteor/templating';
-import { TasksCollection } from '../api/TasksCollection';
-import './App.html';
-
-Template.body.helpers({
-  tasks() {
-    return TasksCollection.find({});
-  },
-});
