39efa215dd0378ee71d46049a4d95488642641b9
denyhs <denilsonh2014@gmail.com>
2021-04-14 15:28:47 -0400

    #606 - Svelte Tutorial
    - Step 6 init

---
+++ src/simple-todos/step06/imports/ui/App.svelte
@@ -0,0 +1,28 @@
+<script>
+  import { TasksCollection } from '../api/TasksCollection';
+  import Task from './Task.svelte';
+  import TaskForm from './TaskForm.svelte';
+
+  $m: tasks = TasksCollection.find({}, { sort: { createdAt: -1 } }).fetch()
+</script>
+
+
+<div class="app">
+    <header>
+        <div class="app-bar">
+            <div class="app-header">
+                <h1>📝️ To Do List</h1>
+            </div>
+        </div>
+    </header>
+
+    <div class="main">
+        <TaskForm />
+
+        <ul class="tasks">
+          {#each tasks as task}
+              <Task key={task._id} task={task} />
+          {/each}
+        </ul>
+    </div>
+</div>
