ecbab56f9436e6d1580b9fed51a29fdbe7d0ee48
denyhs <denilsonh2014@gmail.com>
2021-04-14 13:17:33 -0400

    #606 - Svelte Tutorial
    - Step 2 init

---
+++ src/simple-todos/step02/imports/ui/App.svelte
@@ -0,0 +1,22 @@
+<script>
+  import Task from './Task.svelte';
+
+  const getTasks = () => ([
+    { _id: 'task_1', text: 'This is task 1' },
+    { _id: 'task_2', text: 'This is task 2' },
+    { _id: 'task_3', text: 'This is task 3' },
+  ])
+</script>
+
+
+<div class="container">
+    <header>
+      <h1>Todo List</h1>
+    </header>
+
+    <ul>
+      {#each getTasks() as task}
+          <Task key={task._id} task={task} />
+      {/each}
+    </ul>
+</div>
