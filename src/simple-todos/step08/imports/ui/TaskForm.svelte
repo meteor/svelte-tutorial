1d8c99fa6a039e3a4dc28d3690ef1ddf7900aa2d
denyhs <denilsonh2014@gmail.com>
2021-04-15 11:06:24 -0400

    #606 - Svelte Tutorial
    - Step 8 init

---
+++ src/simple-todos/step08/imports/ui/TaskForm.svelte
@@ -0,0 +1,28 @@
+<script>
+    import { TasksCollection } from '../api/TasksCollection';
+
+    export let user = null;
+    let newTask = '';
+
+    const handleSubmit = () => {
+      // Insert a task into the collection
+      TasksCollection.insert({
+        text: newTask,
+        createdAt: new Date(), // current time
+        userId: user._id,
+      });
+
+      // Clear form
+      newTask = '';
+    }
+</script>
+
+<form class="task-form" on:submit|preventDefault={handleSubmit}>
+    <input
+            type="text"
+            name="text"
+            placeholder="Type to add new tasks"
+            bind:value={newTask}
+    />
+    <button type="submit">Add Task</button>
+</form>
