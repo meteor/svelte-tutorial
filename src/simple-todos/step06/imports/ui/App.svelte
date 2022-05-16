7dd4d949a710f023c0e7ffe08ccf9de2b5040162
hschmaiske <ishenriquealbert@gmail.com>
2022-05-16 16:47:08 -0300

    update step 06

--- src/simple-todos/step06/imports/ui/App.svelte
+++ src/simple-todos/step06/imports/ui/App.svelte
@@ -1,6 +1,5 @@
 <script>
   import { TasksCollection } from '../api/TasksCollection';
-  import { useTracker } from 'meteor/rdb:svelte-meteor-data';
   import Task from './Task.svelte';
   import TaskForm from './TaskForm.svelte';
 
@@ -8,44 +7,45 @@
 
   const hideCompletedFilter = { isChecked: { $ne: true } };
 
-  $m:tasks = TasksCollection.find(hideCompleted ? hideCompletedFilter : {}, { sort: { createdAt: -1 } }).fetch()
-
   let incompleteCount;
   let pendingTasksTitle = '';
+  let tasks = [];
 
-  $: {
-    incompleteCount = useTracker(() => TasksCollection.find(hideCompletedFilter).count());
-    pendingTasksTitle = `${
-      $incompleteCount ? ` (${$incompleteCount})` : ''
-    }`;
+  $m: {
+    tasks = TasksCollection.find(hideCompleted ? hideCompletedFilter : {}, {
+      sort: { createdAt: -1 },
+    }).fetch();
+
+    incompleteCount = TasksCollection.find(hideCompletedFilter).count();
+
+    pendingTasksTitle = `${incompleteCount ? ` (${incompleteCount})` : ''}`;
   }
 
-  const setHideCompleted = value =>  {
+  const setHideCompleted = (value) => {
     hideCompleted = value;
-  }
+  };
 </script>
 
-
 <div class="app">
-    <header>
-        <div class="app-bar">
-            <div class="app-header">
-                <h1>📝️ To Do List {pendingTasksTitle}</h1>
-            </div>
-        </div>
-    </header>
-
-    <div class="main">
-        <TaskForm />
-        <div class="filter">
-            <button on:click={() => setHideCompleted(!hideCompleted)}>
-                {hideCompleted ? 'Show All' : 'Hide Completed'}
-            </button>
-        </div>
-        <ul class="tasks">
-          {#each tasks as task (task._id)}
-              <Task task={task} />
-          {/each}
-        </ul>
+  <header>
+    <div class="app-bar">
+      <div class="app-header">
+        <h1>📝️ To Do List {pendingTasksTitle}</h1>
+      </div>
     </div>
+  </header>
+
+  <div class="main">
+    <TaskForm />
+    <div class="filter">
+      <button on:click={() => setHideCompleted(!hideCompleted)}>
+        {hideCompleted ? 'Show All' : 'Hide Completed'}
+      </button>
+    </div>
+    <ul class="tasks">
+      {#each tasks as task (task._id)}
+        <Task task={task} />
+      {/each}
+    </ul>
+  </div>
 </div>
